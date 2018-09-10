0af65e3fd0c0a18d93ec57ccd38d65c85a2d7954
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-09-10 18:13:08 -0400

    Changed tree file

--- src/learner.jl
+++ src/learner.jl
@@ -37,17 +37,13 @@ end
 
 function predict(X::DataFrame,
                  lnr::OT.OptimalTreeClassifier,
-                 enc2active_constr::Vector{Vector{Int64}})
+                 enc2strategy::Vector{Strategy})
 
     # Predict active_constr
     y = OT.predict(lnr, X)
 
-    # TODO: Fix encoding!
-
-    # Convert encoding to actual active_constr
-    active_constr = [enc2active_constr[y[i]] for i in 1:length(y)]
-
-    return active_constr
+    # Convert predicted encodings to strategies
+    return [enc2strategy[y[i]] for i in 1:length(y)]
 
 end
 
@@ -55,14 +51,14 @@ end
 """
     predict_best_full(X, k, lnr, problem, enc2active_constr)
 
-Predict best active constraints from the best k ones from the tree learner returning
-the full information including the problem solution, the dual, and the time.
+Predict best strategy from the best k ones from the tree learner returning
+the full information including the problem solution, and the time.
 """
 function predict_best_full(X::DataFrame,
                            k::Int64,                                 # k best values
                            lnr::OT.OptimalTreeClassifier,
                            problem::OptimizationProblem,
-                           enc2active_constr::Vector{Vector{Int64}})
+                           enc2strategy::Vector{Strategy})
 
     # Get number of points
 	n_points = size(X, 1)
@@ -71,7 +67,7 @@ function predict_best_full(X::DataFrame,
     proba = OT.predict_proba(lnr, X)
 
 	# Get data to return for each point
-    active_constr = Vector{Vector{Int64}}(n_points)
+    strategy = Vector{Strategy}(n_points)
 	x = Vector{Vector{Float64}}(n_points)
 	y = Vector{Vector{Float64}}(n_points)
 	time = Vector{Float64}(n_points)
@@ -89,11 +85,10 @@ function predict_best_full(X::DataFrame,
 		classes = sortperm(p, rev=true)[1:k]
 
 		# Decode all the classes
-		active_constr_classes = [enc2active_constr[classes[j]] for j in 1:k]
+		strategy_classes = [enc2strategy[classes[j]] for j in 1:k]
 
 		# Get x, y, time for each one of them and store best one
 		x_temp = Vector{Vector{Float64}}(k)
-		y_temp = Vector{Vector{Float64}}(k)
 		time_temp = Vector{Float64}(k)
 		infeas_temp = Vector{Float64}(k)
         cost_temp = Vector{Float64}(k)
@@ -102,8 +97,7 @@ function predict_best_full(X::DataFrame,
 		for j = 1:k
 
             # Solve problem
-            x_temp[j], y_temp[j], time_temp[j] = solve(problem,
-                                                       active_constr_classes[j])
+            x_temp[j], time_temp[j] = solve(problem, strategy_classes[j])
 
 			# Compute infeasibility
 			infeas_temp[j] = infeasibility(x_temp[j], problem)
@@ -133,27 +127,27 @@ function predict_best_full(X::DataFrame,
         #  end
 
 		# Store value we are interested in
-        x[i], y[i] = x_temp[idx_pick], y_temp[idx_pick]
+        x[i] = x_temp[idx_pick]
         time[i] = sum(time_temp)   # Sum all the solve times
-        active_constr[i] = active_constr_classes[idx_pick]
+        strategy[i] = strategy_classes[idx_pick]
 
     end
 
-    # Return x, y, time, and active_constr for all the points
-    return x, y, time, active_constr
+    # Return x, time, and strategy for all the points
+    return x, time, strategy
 
 end
 
 """
     predict_best(X, k, lnr, problem, enc2active_constr)
 
-Predict best active constraints from the best k ones from the tree learner.
+Predict best strategy from the best k ones from the tree learner.
 """
 function predict_best(X::DataFrame,
                       k::Int64,                                 # k best values
                       lnr::OT.OptimalTreeClassifier,
                       problem::OptimizationProblem,
-                      enc2active_constr::Vector{Vector{Int64}})
-    _, _, _, active_constr = predict_best_full(X, k, lnr, enc2active_constr)
-    return active_constr
+                      enc2strategy::Vector{Strategy})
+    _, _, strategy = predict_best_full(X, k, lnr, enc2strategy)
+    return strategy
 end
