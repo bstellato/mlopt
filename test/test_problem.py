fb6fb58452140e282b4fe28690893a46d0a4a165
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-10-15 15:41:33 -0400

    Added test to check if perturbed problem is the same as original

--- test/test_problem.py
+++ test/test_problem.py
@@ -1,8 +1,11 @@
 import unittest
 import numpy as np
+import numpy.testing as npt
 import cvxpy as cp
 import mlopt as mo
+from mlopt.settings import DEFAULT_SOLVER
 from mlopt.settings import TOL
+from copy import deepcopy
 
 
 class TestProblem(unittest.TestCase):
@@ -33,3 +36,32 @@ class TestProblem(unittest.TestCase):
         viol_manual = np.linalg.norm(np.maximum(A.dot(x_val) - b,
                                                 0))
         self.assertTrue(abs(viol_cvxpy - viol_manual) <= TOL)
+
+    def test_solve_perturbation(self):
+        """Solve cvxpy problem vs perturbed problem.
+           Expect similar solutions."""
+        np.random.seed(1)
+        n = 5
+        m = 15
+        x = cp.Variable(n)
+        c = np.random.randn(n)
+        A = np.random.randn(m, n)
+        b = np.random.randn(m)
+        cost = c * x
+        constraints = [A * x <= b]
+
+        cvxpy_problem = cp.Problem(cp.Minimize(cost), constraints)
+        cvxpy_problem.solve(solver=DEFAULT_SOLVER, verbose=True)
+        x_cvxpy = deepcopy(x.value)
+        cost_cvxpy = cost.value
+        problem = mo.OptimizationProblem(cp.Minimize(cost), constraints)
+        results = problem.solve(verbose=True)
+        x_problem = deepcopy(x.value)
+        cost_problem = cost.value
+
+        npt.assert_almost_equal(x_problem,
+                                x_cvxpy,
+                                decimal=TOL)
+        npt.assert_almost_equal(cost_problem,
+                                cost_cvxpy,
+                                decimal=TOL)
