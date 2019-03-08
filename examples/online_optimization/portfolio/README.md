cf1988e72d278ca5d97a8abe416124c17bf1ecb5
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-03-08 17:53:20 -0500

    Tried portfolio simulation. Too many strategies

---
+++ examples/online_optimization/portfolio/README.md
@@ -0,0 +1,18 @@
+# Multiperiod portfolio investment example
+
+
+## Download data
+
+Before running any simulation or learning you need to download the data from `quandl.com`. You can do so by executing the script
+
+```
+python download_data.py
+```
+
+Note that you need `pytables` and `quandl` packages for it to work.
+
+
+
+
+
+
--- examples/online_optimization/portfolio/benchmark_simulation.py
+++ examples/online_optimization/portfolio/benchmark_simulation.py
@@ -4,6 +4,7 @@ from simulation.policy import Hold, Optimal
 import datetime as dt
 import logging
 import matplotlib.pylab as plt
+
 # Write basic simulation code with basic strategies
 
 
@@ -24,14 +25,14 @@ with pd.HDFStore("./data/risk_data.h5") as risk_data:
 
 
 # Initial portfolio allocation (equally divided over the assets
-#  w_init = pd.Series(index=returns.columns, data=1)
-#  w_init.USDOLLAR = 0.
-#  w_init /= sum(w_init)
+w_init = pd.Series(index=returns.columns, data=1)
+w_init.USDOLLAR = 0.
+w_init /= sum(w_init)
 
 
 # Point all on apple
-w_init = pd.Series(index=returns.columns, data=0.)
-w_init['AAPL'] = 1.
+#  w_init = pd.Series(index=returns.columns, data=0.)
+#  w_init['AAPL'] = 1.
 
 
 # Invest funds
@@ -39,9 +40,9 @@ funds = 1e4
 h_init = w_init * funds
 
 # Times
-start = dt.date(2008, 1, 1)
+t_start = dt.date(2008, 1, 1)
 #  end = dt.date(2017, 1, 1)
-end = dt.date(2013, 1, 1)
+t_end = dt.date(2013, 1, 1)
 
 
 # For loop propagating the market as in simulator.run_backtest
@@ -54,9 +55,11 @@ simulator = MarketSimulator(returns=returns,
 #                                    policy=Hold(), log_level=logging.WARNING)
 #  v_hold = [h.sum() for _, h in results_hold['h'].iterrows()]
 
-# SinglePeriod policyy
-op_policy = Optimal(returns_estimates, risk, periods=2)
-op_results = simulator.backtest(h_init, t_start=start, t_end=end,
+# Optimal polict
+T_periods = 2
+op_policy = Optimal(returns_estimates, risk, periods=T_periods)
+op_results = simulator.backtest(h_init, t_start=t_start,
+                                t_end=t_end,
                                 policy=op_policy, log_level=logging.WARNING)
 
 print(op_results['stats'])
@@ -79,6 +82,6 @@ print(op_results['stats'])
 #  # TODO
 #  - [x] Write check for randomness! Finish it in tests
 #  - [x] Write stats risk/excess returns
-#  - [ ] Write complete loop
+#  - [x] Write complete loop
 #  - [ ] Write MLOPT full constraints selection
 #  - [ ]
